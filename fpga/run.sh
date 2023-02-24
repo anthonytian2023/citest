16d964882ec1c483aeda0c83f1d9d0c2a8f34077
anthony.tian <anthony.tian@cixtech.com>
2023-02-24 09:22:52 +0800

    Update run script
    Signed-off-by: anthony.tian <anthony.tian@cixtech.com>

--- fpga/run.sh
+++ fpga/run.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-
-if [ $1 = 'AP' ]; then
-    cd /home/jackeydu/workspace/git/ltp/testcases/cix_tests_suite/fpgaci/ap
-elif [ $1 = 'apxip' ]; then
-    cd /home/jackeydu/workspace/git/ltp/testcases/cix_tests_suite/brom/ap_xip
-else
-    cd /home/jackeydu/workspace/git/ltp/testcases/cix_tests_suite/brom/ap_xip_stress
-fi
+tdir=$HOME/CI/$1
+cd $tdir
 export PATH=/home/jackeydu/.local/bin:$PATH
 pytest
 rp=`ls -tl reports/*html | head -n 1 | cut -d / -f2`
+# need to modify it to related with jobid
 cp reports/$rp /home/jackeydu/jenkins/workspace/report/reports.html
