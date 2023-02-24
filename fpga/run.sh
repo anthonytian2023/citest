59c671744ab98988f4bacf0ab828d02af0702ff7
anthony.tian <anthony.tian@cixtech.com>
2023-02-24 10:40:52 +0800

    Update scripts
    Signed-off-by: anthony.tian <anthony.tian@cixtech.com>

--- fpga/run.sh
+++ fpga/run.sh
@@ -2,6 +2,7 @@
 tdir=$HOME/CI/$1
 cd $tdir
 export PATH=/home/jackeydu/.local/bin:$PATH
+cd ltp/testcases/cix_tests_suite/fpgaci/pengine
 pytest
 rp=`ls -tl reports/*html | head -n 1 | cut -d / -f2`
 # need to modify it to related with jobid
--- fpga/scm.sh
+++ fpga/scm.sh
@@ -3,8 +3,8 @@ tdir=$HOME/CI/$1
 if [ $2 == setup ]; then
     mkdir -p $tdir
     cd $tdir
-    git clone --depth 1 ssh://git@gitmirror.cixtech.com/cix_test/ltp
+    git clone --depth 1 ssh://git@gitmirror.cixtech.com/cix_test/ltp -b cix_master
     cd -
 else
-    rm $tdir
+    rm -rf $tdir
 fi
