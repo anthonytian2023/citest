02134264794984a26765cc7a1c65a5e76db5df6a
anthony.tian <anthony.tian@cixtech.com>
2023-02-27 15:31:37 +0800

    skip large file
    Signed-off-by: anthony.tian <anthony.tian@cixtech.com>

--- fpga/scp.py
+++ fpga/scp.py
@@ -22,6 +22,8 @@ cid.expect([pexpect.EOF, pexpect.TIMEOUT])
 fList = os.popen('ls ' + sys.argv[1]).read().rstrip().split("\n")
 print(fList)
 for fl in fList:
+    if "rootfs.ext4.hex" in fl:
+        continue
     print("scp %s svc.fpgatest@fpga03.cixcomputing.cn:%s" % (sys.argv[1]+'/'+fl, sys.argv[2]))
 
     cid = pexpect.spawn("scp %s svc.fpgatest@fpga03.cixcomputing.cn:%s" %\
