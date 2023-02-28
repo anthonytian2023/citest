3bba8ed427ebac36a06f0023c548f7c5e97cd54c
anthony.tian <anthony.tian@cixtech.com>
2023-02-28 10:51:48 +0800

    Jenkinsfile issue fix
    Signed-off-by: anthony.tian <anthony.tian@cixtech.com>

--- fpga/run.sh
+++ fpga/run.sh
@@ -4,7 +4,7 @@ cd $tdir
 export PATH=/home/jackeydu/.local/bin:$PATH
 cd ltp/testcases/cix_tests_suite/fpgaci/pengine
 ttl=$2
-dsc="$_Auto_Test"
+dsc="$2_Auto_Test"
 rFlag="--report=report.html --title=$ttl --tester='SW-Test-Robot' --desc=$dsc"
 if [ -n "$7" ]; then
     kFlag="-k $7"
