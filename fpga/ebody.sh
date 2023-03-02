6ed273c928b9f50712ff7c1160a6d00ca16d8413
anthony.tian <anthony.tian@cixtech.com>
2023-03-02 22:47:35 +0800

    enhance
    Signed-off-by: anthony.tian <anthony.tian@cixtech.com>

--- fpga/ebody.sh
+++ fpga/ebody.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 rstr="Dear All,\n\n\tBelow is the summary:"
 while read line; do
-    rstr="${rstr}\n\n\t"
+    rstr="${rstr}\n\t"
     rstr="${rstr}${line}"
 done < $1
+rplink="https://ci.cixcomputing.com/job/$2/AP_5fBSP-Test-Report/"
+rstr="${rstr}\n\t"
+rstr="${rstr}The full report link is: ${rplink}"
 rstr="${rstr}\n\n"
-rstr="${rstr}\n\n\t"
 rstr="${rstr}SW-Test-Robot"
 
-echo $rstr
+echo -e $rstr
     
 
