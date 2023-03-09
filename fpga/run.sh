4ac4c88fa314b0d80fc5f09d28d4c13977e64270
anthony.tian <anthony.tian@cixtech.com>
2023-03-09 10:11:42 +0800

    Update summary file
    Signed-off-by: anthony.tian <anthony.tian@cixtech.com>

--- fpga/run.sh
+++ fpga/run.sh
@@ -16,3 +16,4 @@ pytest $rFlag $kFlag
 rp=`ls -tl reports/*html | head -n 1 | cut -d / -f2`
 # need to modify it to related with jobid
 cp reports/$rp /home/jackeydu/jenkins/workspace/report/reports.html
+cp result.txt /home/jackeydu/jenkins/workspace/report/
